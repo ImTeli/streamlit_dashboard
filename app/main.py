db0a19d80855f208563c05b18c28f7d19df6c3f9
Hiago T. <hiagobvt@live.com>
2024-08-11 19:30:02 -0300

    feat: Orientação a Objetos

--- app/main.py
+++ app/main.py
@@ -1,29 +1,47 @@
 import streamlit as st
-from utils.data_import import df
-from utils.format_number import format_number as fn
-import utils.charts as chart
+from utils.data_import import generate_df
+from utils.df_class import Teliframe
 
 
-st.set_page_config(layout="wide", page_title="")
+st.set_page_config(layout="wide", page_title="Proposta Indecorosa", page_icon="🌛")
 st.title("Dashboard de Vendas :shopping_trolley:")
+
+df = generate_df()
+tayframe = Teliframe(df)
+
+st.sidebar.title("Filtros")
+
+filtro_vendedor = st.sidebar.multiselect("Vendedores",df["Vendedor"].unique())
+if filtro_vendedor:
+    df = df[df["Vendedor"].isin(filtro_vendedor)]
+filtro_pagamento = st.sidebar.multiselect("Tipo de pagamento",df["Tipo de pagamento"].unique())
+if filtro_pagamento:
+    df = df[df["Tipo de pagamento"].isin(filtro_pagamento)]
+filtro_local = st.sidebar.multiselect("Local da compra", df["Local da compra"].unique())
+if filtro_local:
+    df = df[df["Local da compra"].isin(filtro_local)]
+
+tayframe.update(df)
 
 aba1, aba2, aba3 = st.tabs(["Dataset", "Receita", "Vendedores"])
 
 with aba1:
-    st.dataframe(df)
+    st.dataframe(tayframe.df)
 
 with aba2:
     coluna1, coluna2 = st.columns(2)
     with coluna1:
-        st.metric("Receita Total", fn(df["Preço"].sum(), prefix="R$"))
-        st.plotly_chart(chart.grafico_map_estado, use_container_width=True)
-        st.plotly_chart(chart.grafico_rec_estado, use_container_width=True)
+        st.metric("Receita Total", tayframe.format_number(df["Preço"].sum(), prefix="R$"))
+        st.plotly_chart(tayframe.map_estado_chart(), use_container_width=True)
+        st.plotly_chart(tayframe.rec_estado_chart(), use_container_width=True)
     with coluna2:
-        st.metric("Quantidade de vendas", fn(df.shape[0]))
-        st.plotly_chart(chart.grafico_rec_mensal, use_container_width=True)
-        st.plotly_chart(chart.grafico_rec_categoria, use_container_width=True)
+        st.metric("Quantidade de vendas", (df.shape[0]))
+        st.plotly_chart(tayframe.rec_mensal_chart(), use_container_width=True)
+        st.plotly_chart(tayframe.rec_categoria_chart(), use_container_width=True)
 
 with aba3:
     coluna1, coluna2 = st.columns(2)
     with coluna1:
-        st.plotly_chart(chart.grafico_rec_vendedores)
+        st.plotly_chart(tayframe.rec_vendedores_chart())
+    with coluna2:
+        st.plotly_chart(tayframe.vendas_vendedores_chart())
---
+++ app/utils/df_class.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import plotly.express as px
+
+class Teliframe:
+    def __init__(self,df) -> None:
+        self.df = df
+        self.rec_estado = None
+        self.rec_mensal = None
+        self.rec_categoria = None
+        self.df_vendedores = None
+        self.update(self.df)
+        
+    def update(self, df):
+        self.df = df
+        df_rec_estado_agg = self.df.groupby('Local da compra')[["Preço"]].sum()
+        self.rec_estado = self.df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']]
+        self.rec_estado = self.rec_estado.merge(df_rec_estado_agg, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
+        self.rec_mensal = self.df.set_index('Data da Compra').groupby(pd.Grouper(freq='ME'))['Preço'].sum().reset_index()
+        self.rec_mensal['Ano'] = self.rec_mensal['Data da Compra'].dt.year
+        self.rec_mensal['Mes'] = self.rec_mensal['Data da Compra'].dt.month_name()
+        self.rec_categoria = self.df.groupby("Categoria do Produto")[["Preço"]].sum().sort_values("Preço", ascending=True)
+        self.df_vendedores = pd.DataFrame(self.df.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
+
+    def map_estado_chart(self):
+        return px.scatter_geo(
+            self.rec_estado,
+            lat='lat',
+            lon='lon',
+            scope='south america',
+            size='Preço',
+            template='seaborn',
+            hover_name='Local da compra',
+            hover_data={'lat': False, 'lon': False},
+            title='Receita por estado',
+            height=450,
+            width=900,
+            basemap_visible=True,
+        )
+
+    def rec_mensal_chart(self):
+        return px.line(
+            self.rec_mensal,
+            x="Mes",
+            y="Preço",
+            color='Ano',
+            title='Receita Mensal'
+        )
+    
+    def rec_estado_chart(self):
+        return px.bar(
+            self.rec_estado.head(5),
+            x='Local da compra',
+            y='Preço',
+            text_auto=True,
+            title='Top 10 Receita por Estado'
+        )
+
+    def rec_categoria_chart(self):
+        return px.bar(
+            self.rec_categoria.head(5),
+            orientation='h',
+            text_auto=True,
+            title='Top 10 Categorias por Receita'
+        )
+
+    def rec_vendedores_chart(self):
+        return px.bar(
+            self.df_vendedores.sort_values('sum', ascending=False).head(5).iloc[::-1],
+            x='sum',
+            y=self.df_vendedores.sort_values('sum', ascending=False).head(5).iloc[::-1].index,
+            text_auto=True,
+            title='Top 10 Vendedores por Receita'
+        )
+
+    def vendas_vendedores_chart(self):
+        return px.bar(
+            self.df_vendedores[["count"]].sort_values('count').tail(5),
+            x='count',
+            y=self.df_vendedores[["count"]].sort_values('count').tail(5).index,
+            text_auto=True,
+            title='Top 10 Vendedores por Vendas'
+        )
+
+    def format_number(self, value, prefix = ''):
+        if value < 1000:
+            return f'{prefix} {value:.2f}'
+        elif value < 1000000:
+            value /= 1000
+            return f'{prefix} {value:.2f} mil'
+        elif value < 1000000000:
+            value /= 1000000
+            return f'{prefix} {value:.2f} milhões'
+        elif value < 1000000000000:
+            value /= 1000000000
+            return f'{prefix} {value:.2f} bilhões'
+        elif value < 1000000000000000:
+            value /= 1000000000000
+            return f'{prefix} {value:.2f} trilhões'
